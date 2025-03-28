7886be6a0e473d867e2768e7e7a1fbf931b56c1c
HolyG <gggigi111@gmail.com>
2025-03-28 21:03:24 +0100

    wrongThinking=Reset

--- src/darts.ts
+++ src/darts.ts
@@ -1,22 +1,9 @@
 export function calcPoints(hits: string): number {
-
-//const sectors = [20, 1, 18, 4, 13, 6, 10, 15, 2, 17, 3, 19, 7, 16, 8, 11, 14, 9, 12, 5]; // 20 Sektoen sollten stimmen 
-
-
-  return hits = hitA + hitB + hitC
+  console.log(hits);
+  throw new Error("not implemented yet")
 }
 
 export function possibleCheckout(x: number): string {
   console.log(x);
   throw new Error("not implemented yet");
 }
-
-/* Radiusbereiche
-Bereich	      Radius (von - bis, relativ)	  Multiplier	  Name
-Inner Bull	  0.0 – 0.05	                  2	            "Inner Bull"
-Outer Bull	  0.05 – 0.1	                  1	            "Outer Bull"
-Single inner	0.1 – 0.6	                    1	            "S"
-Triple	      0.6 – 0.65	                  3	            "T"
-Single outer	0.65 – 0.95	                  1	            "S"
-Double	      0.95 – 1.0	                  2	            "D" 
-*/
