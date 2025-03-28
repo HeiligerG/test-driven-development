4618e30529597586a804814a7ac3ebfe9664c92b
HolyG <gggigi111@gmail.com>
2025-03-28 17:20:59 +0100

    Aufgabe3-TDD-productive-isbn13

--- src/isbn13.ts
+++ src/isbn13.ts
@@ -1,4 +1,14 @@
-export function isValid(isbn13: string): boolean {
-  console.log(isbn13);
-  throw new Error("not implemented yet");
+export function isValid(isbn: string): boolean {
+  const digits = isbn.replace(/[\s-]/g, ""); // mit RegEx -> Reguar expresion
+
+  if (!/^\d{13}$/.test(digits)) return false; // ebenfals mit RegEx
+
+  const sum = digits
+    .split("")
+    .map(Number)
+    .reduce((acc, digit, i) => acc + digit * (i % 2 === 0 ? 1 : 3), 0); //kurz aber perfekt, glaube ich
+
+  return sum % 10 === 0;
 }
+
+// trow-exeption müsste vlt. noch implementiert werden
